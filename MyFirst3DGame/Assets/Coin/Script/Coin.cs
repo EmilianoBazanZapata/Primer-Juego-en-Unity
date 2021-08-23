03f64bc667fb7d15a0ffd698f6c6225a447f6ed9
Emiliano Bazan Zapata <emilianobz546@gmail.com>
2021-08-23 19:52:46 -0300

    Recolectando la Moneda

--- MyFirst3DGame/Assets/Coin/Script/Coin.cs
+++ MyFirst3DGame/Assets/Coin/Script/Coin.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
+﻿//usings necesarios para que todo funcione 
+//de forma normal
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+  declaracion de la clase
+*/
 public class Coin : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        //Debug.Log("El Juego Comenzo");
+    }
+  
+    // Update is called once per frame
+    private void Update()
+    {
+        //Debug.Log("El Juego Esta Funcionando");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+      if(other.CompareTag("Player"))
+      {
+        //Debug.Log("se provoco una colision");
+        Destroy(gameObject);
+      }
     }
 }
