91bc5614a0cb940a1e72e82194e2cd5b91d80eeb
Emiliano Bazan Zapata <emilianobz546@gmail.com>
2021-08-23 20:46:17 -0300

    Agregando una Cuneta Atras del Tiempo Limite

--- MyFirst3DGame/Assets/Scripts/Timer.cs
+++ MyFirst3DGame/Assets/Scripts/Timer.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     public float MaxTime = 60f;
+    private float CountDown = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        CountDown = MaxTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //restando tiempo
+        CountDown -= Time.deltaTime;
+        Debug.Log(CountDown);
+        if(CountDown<=0)
+        {
+            Debug.Log("Perdiste , Sos de Manera Perdiste");
+            Coin.CoinsCount = 0;
+            SceneManager.LoadScene("MainScene");
+        }
     }
 }
